fca63b3ddf01ed5e4cb5514e73c7246985b25143
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-07-02 16:40:09 +0500

    created about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint neque omnis fugiat nisi sed, aliquam assumenda molestias doloribus natus ab maxime qui beatae molestiae ullam quaerat porro aperiam tenetur commodi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus soluta doloremque ea tempore harum itaque amet ipsa architecto, a molestiae minima modi blanditiis atque necessitatibus error alias commodi porro quibusdam! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia voluptates esse deserunt quae molestias, consequuntur quaerat! Dolor alias, libero rerum enim ab nulla facilis perferendis! Pariatur expedita eveniet quis unde.
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quae dolorem ab hic omnis accusamus est porro molestiae ad voluptates dolores consequatur, sequi officiis! Fugiat eos perspiciatis sed quia minima!
+        Impedit, perspiciatis, qui praesentium eos itaque obcaecati voluptates necessitatibus fugiat quidem saepe similique explicabo? Hic, alias praesentium amet ratione accusamus nemo. Autem nisi odit enim cum placeat, distinctio dignissimos officia.
+        Tempore laudantium ipsa, sequi consequuntur quisquam laboriosam in hic culpa molestiae vitae maiores facere sint dignissimos totam blanditiis quidem mollitia officiis atque dolorem sed quam architecto rem dicta nihil? Nulla.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius alias vero officia animi ipsum expedita consectetur esse, earum dolore incidunt sed minima reiciendis. Recusandae saepe error numquam aperiam, et iste.
+        Est, reiciendis? Consequatur, iste totam temporibus incidunt quas odio porro quibusdam magnam nihil? Ducimus numquam sed officiis autem necessitatibus nesciunt quasi obcaecati quam quidem fuga repellendus, aperiam reiciendis totam alias!
+        Porro ex voluptatum placeat dolorum assumenda! Sed, blanditiis suscipit? Quibusdam, consequuntur, ad aliquam sit et cum harum totam molestiae, voluptatibus eum dolorem placeat ut ipsam mollitia magni. Obcaecati, quam tempora.
+        Fugiat ullam facere harum doloremque molestiae hic aspernatur, vitae voluptatum laudantium odit, enim, veritatis repudiandae voluptate eligendi perferendis. A similique perferendis impedit repellat, quaerat animi natus alias vitae id unde.</p>
+    </div>
+  )
+}
